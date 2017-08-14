a67b8e31ddebacf68a689c15af8f6417a7fba203
Kronossaurus <brvictor.chess@hotmail.com>
2017-08-13 21:39:36 -0300

    Converting to python3

--- bissecao.py
+++ bissecao.py
@@ -9,15 +9,15 @@ def bissecao (px, pf, x0, x1, it):
     f = sympify(pf)
 
     for i in range(0, it):
-        print "Iteração", i+1
+        print ("Iteração", i+1)
         fx0   = f.subs(x, x0)
         fx1   = f.subs(x, x1)
         meio  = x0 + (x1-x0)/2
         fmeio = f.subs(x, meio)
         
-        print "x0   =", x0, "\t f(x0) =", fx0 
-        print "x1   =", x1, "\t f(x1) =", fx1
-        print "meio =", meio, "\t f(meio) =", fmeio
+        print ("x0   =", x0, "\t f(x0) =", fx0) 
+        print ("x1   =", x1, "\t f(x1) =", fx1)
+        print ("meio =", meio, "\t f(meio) =", fmeio)
         print
         
         if fmeio * fx0 > 0:
@@ -26,6 +26,6 @@ def bissecao (px, pf, x0, x1, it):
             x1 = meio
         else:
             break
-    print "Erro estimado:", abs(x0-x1)/2**it
+    print ("Erro estimado:", abs(x0-x1)/2**it)
 
 bissecao ('x', 'ln(x) * cos(x)', 1.1, 2.1, 15)
