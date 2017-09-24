ef3688eb9bc0802065e1f91d98508890006800db
andressaumetsu <andressa.umetsu@gmail.com>
2017-09-24 20:07:16 -0300

    modificado bissecao de acordo com enunciado do trabalho

--- bissecao.py
+++ bissecao.py
@@ -4,28 +4,36 @@
 from sympy import *
 
 #símbolo, função, limite superior, limite inferior, número de iterações
-def bissecao (px, pf, x0, x1, it):
+def bissecao (px, pf, x0, x1):
     x = Symbol(px)
     f = sympify(pf)
-    print ("f(x) =", pf, "\n")
+    i = 0
 
-    for i in range(0, it):
-        print ("Iteração", i+1)
+    print ("f(x) =", pf, "\n")
+    
+    erroP = abs(x0 - x1)/abs(x1)
+    while erroP > 0.00001:
+        i=i+1
+        
+        print ("\nIteração ", i)
+
         fx0   = f.subs(x, x0).evalf()
         fx1   = f.subs(x, x1).evalf()
         meio  = x0 + (x1-x0)/2
         fmeio = f.subs(x, meio).evalf()
+        erroP = (abs(x0 - x1)/abs(x1))*100
 
+        print ("erro =", erroP, '\n')
         print ("x0   =", x0, "\t f(x0) =", fx0)
         print ("x1   =", x1, "\t f(x1) =", fx1)
         print ("meio =", meio, "\t f(meio) =", fmeio, '\n')
-
+        
         if fmeio * fx0 > 0:
             x0 = meio
         elif fmeio * fx1 > 0:
             x1 = meio
         else:
             break
-    print ("Erro estimado:", abs(x0-x1)/2**it)
+    # print ("Erro estimado :", abs(x0-x1)/2**it)
 
-bissecao ('x', 'ln(x) * cos(x)', 1.1, 2.1, 15)
+bissecao ('x', '1 + x*sin(x)', 0, 3*pi.evalf()/2)
