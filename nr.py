fdf52384934470d7acedf9ab4a6d9ee0e96fb105
nobreconfrade <willpereirabr@gmail.com>
2017-09-24 21:18:58 -0300

    com as especificacoes do trabalho

--- nr.py
+++ nr.py
@@ -2,20 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from sympy import *
+import math
 
-#símbolo, função, valor inicial, número de iterações
-def nr (px, pf, x0, it):
+#símbolo, função, valor inicial
+def nr (px, pf, x0):
     x = Symbol(px)
     f = sympify(pf)
     df = diff(pf)
     print ("f(x) =", pf, "\n")
+    i = 0
+    fx0 = 1
 
-    for i in range(0, it):
+    while abs(fx0) > 0.00001:
         print ("Iteração", i+1)
         fx0  =  f.subs(x, x0).evalf()
         dfx0 = df.subs(x, x0).evalf()
         x0   = x0 - fx0/dfx0
-
+        i+=1
         print ("x =", x0, "\t f(x0) =", fx0, '\n')
 
-nr ('x', 'ln(x) * cos(x)', 2.0, 15)
+nr ('x', '1 + x * sin(x)', 5/2)
