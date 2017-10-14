f3673bc0a1d8bbfbdcd01200eca5df70eadaf85b
Kronossaurus <brvictor.chess@hotmail.com>
2017-10-14 19:10:16 -0300

    Interpolação de Lagrange

---
+++ interpolacao/lagrange.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sympy import *
+x = Symbol('x')
+
+#lista de pontos
+def lagrange (lista):
+    n = len(lista)
+    poli = 0
+    L = 0
+    for a,b in lista:
+        L = b
+        for c,d in lista:
+            if(a != c):
+                L *= (x - c)/(a - c)
+        print("Li(x) =", L)
+        poli += L
+
+    poli = simplify(poli)
+    print(poli)
+    return poli
+
+lista = [(2,3), (3,6), (4,10)]
+# lista = [(-1,-1), (0,0), (1,1), (8,2)]
+lagrange(lista)
